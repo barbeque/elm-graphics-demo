3db03c1d4c5b0ddbd9bce31806e40f235dc6454f
mike stedman <github@ravuya.com>
2017-05-31 15:22:15 -0600

    add rotation transform

--- main.elm
+++ main.elm
@@ -1,13 +1,17 @@
 module Main exposing (..)
 
 import Color exposing (..)
-import Element exposing (..)
 import Collage exposing (..)
 import Html exposing (Html)
+import Element exposing (toHtml)
+import Transform exposing (rotation)
 
 main : Html msg
 main = collage 300 300
-    [makeSquare blue 50]
+    [
+      groupTransform (rotation 1.7)
+      [makeSquare blue 50]
+    ]
     |> toHtml
     
 makeSquare color size =
